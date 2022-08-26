0fb4af554cb92df43d15b4cc5bf28282fec6a420
zry <2399277159@qq.com>
2022-08-26 17:29:42 +0800

    结构搭建

--- src/components/content/mainTabbar/MainTabBar.vue
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -7,8 +7,6 @@
     </tab-bar-item>
     <tab-bar-item path="/category">
       <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt="">
-
-
       <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg" alt="">
       <div slot="item-text">分类</div>
     </tab-bar-item>
