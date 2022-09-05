a6575a71ea1fc0aa486dc414a79b2724a4812370
zry <2399277159@qq.com>
2022-09-05 19:41:23 +0800

    f

---
+++ src/views/detail/childComps/DetailBottomBar.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="bottom-bar">底部工具栏</div>
+</template>
+
+<script>
+    export default {
+        name: "DetailBottomBar"
+    }
+</script>
+
+<style scoped>
+  .bottom-bar {
+    position: fixed;
+    height: 49px;
+    margin-bottom: 49px;
+
+  }
+
+</style>
